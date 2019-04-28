e424d038fa24c56b86dd024c818c912862985445
yms <mi_msyuan@outlook.com>
2019-04-28 16:35:49 +0800

    状态提升

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import Counters from './components/counters'
 
 class App extends Component {
-  state = {  }
+  state = { 
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ]
+   }
+   handleDelete = (counterId) => {
+     const counters = this.state.counters.filter(c => c.id !== counterId);
+     this.setState({ counters });
+   } 
+   handleIncrement = (counter) => {
+     const counters = this.state.counters.map(c => {
+      if (c.id === counter.id) c.value += 1;
+      return c;
+     });
+     this.setState({ counters });
+   }
+   handleReset = () => {
+     const counters = this.state.counters.map(c => { c.value = 0; return c });
+     this.setState({ counters });
+   }
   render() { 
     return ( 
-      <Counters />
+      <Counters
+      counters={this.state.counters}
+      onDelete={this.handleDelete}
+      onIncrement={this.handleIncrement}
+      onReset={this.handleReset}
+      />
      );
   }
 }
